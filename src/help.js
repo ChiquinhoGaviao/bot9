595bca2210d27f48885fb8c8337db2d66bf35d66
ChiquinhoGavião <hengustavo45@gmail.com>
2021-02-24 22:58:42 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,286 @@
+const help = (prefix) => {
+	return `
+
+饾棔饾棦饾棫 ChiquinhoBot:
+
+ *O BOT SE ENCONTRA: ON-LINE*
+ 鈻柆鈻柆鈻柆鈻�愨槳銆戔柆鈻柆鈻柆鈻柆
+ *DONO*: ChiquinhoBot
+ 鈻柆鈻柆鈻柆鈻�愨槳銆戔柆鈻柆鈻柆鈻柆
+ *PREFIXO*:[ / ]
+ 鈻柆鈻柆鈻柆鈻�愨槳銆戔柆鈻柆鈻柆鈻柆
+ *WHATSAPP*: wa.me/554184537851
+ 鈻柆鈻柆鈻柆鈻�愨槳銆戔柆鈻柆鈻柆鈻柆
+ *GRUPO OFC*:  https://chat.whatsapp.com/DigleWrJkkv3vKYufBExVW
+ *GRUPO 虏*: https://chat.whatsapp.com/DigleWrJkkv3vKYufBExVW
+ 鈻柆鈻柆鈻柆鈻�愨槳銆戔柆鈻柆鈻柆鈻柆
+
+
+        饾棖饾棦饾棤饾棓饾棥饾棗饾棦饾棪 饾棓 饾棔饾棓饾棞饾棲饾棦:
+
+      饾棖饾棦饾棤饾棓饾棥饾棗饾棦饾棪:
+
+鉃€�愷潡狆潡橉潡燄潡涴潡潡ヰ潡橉潡︺��
+
+鉃� *${prefix}Sticker* [Faz figurinha]
+鉃� *${prefix}play* [nome da m煤sica]
+鉃� *${prefix}toimg* [converter figurinha em imagem]
+鉃� *${prefix}wame* [link do seu whatsapp]
+鉃� *${prefix}meme* [memes aleat贸rios]
+鉃� *${prefix}nabutojokes* [memes2]
+鉃� *${prefix}tts pt* [seu texto]
+鉃� *${prefix}ping* [velocidade]
+鉃� *${prefix}owner ou dono* [info do criador]
+
+鉃€�愷潡○潡潡潡潡︺��
+
+鉃� *${prefix}animecry*
+鉃� *${prefix}chentai [premium]*
+鉃� *${prefix}gcpf [premium]*
+鉃� *${prefix}gay [@]*
+鉃� *${prefix}gbin [premium]*
+鉃� *${prefix}pack [premium]*
+鉃� *${prefix}destrava [premium]*
+鉃� *${prefix}gpessoa [premium]*
+鉃� *${prefix}spamcall*
+鉃� *${prefix}play (nome da msc)*
+
+鉃€�愷潡ｐ潡旔潡ヰ潡� 饾棜饾棩饾棬饾棧饾棦饾棪銆�
+
+鉃� *${prefix}closegc* [fechar grupo]
+鉃� *${prefix}opengc* [abrir grupo]
+鉃� *${prefix}antilink* 1 [anti link]
+鉃� *${prefix}antiracismo on* [anti racismo]
+鉃� *${prefix}banir* [banir membro]
+鉃� *${prefix}admins* [lista se administradores]
+鉃� *${prefix}marcar* [marcar todos membros]
+鉃� *${prefix}linkgp* [link do grupo]
+鉃� *${prefix}promover* [dar adm]
+鉃� *${prefix}rebaixar* [tirar adm]
+鉃� *${prefix}bemvindo* 1 [recusso de boas vindas]
+鉃� *${prefix}grupoinfo* [info]
+鉃� *${prefix}setdesc* [trocar descri莽茫o]
+鉃� *${prefix}setfoto* [mudar foto]
+鉃� *${prefix}porno* [porno]
+鉃� *${prefix}mia* [fotos da mia]
+
+鉃€�愷潡滒潡○潡ю潡橉潡ヰ潡旔潡氿潡滒潡ャ��
+鉃�
+鉃� *${prefix}figu*
+鉃� *${prefix}toimg*
+鉃� *${prefix}nabutojokes (memes aleat贸rios)*
+鉃� *${prefix}memeindo*
+鉃� *${prefix}tts*
+鉃� *${prefix}lolih [on]*
+鉃� *${prefix}nsfwloli [off]*
+鉃� *${prefix}url2img*
+鉃� *${prefix}leens [na legenda]*
+鉃� *${prefix}wait [na legenda]*
+鉃� *${prefix}setprefix*
+鉃�
+鉃€�愷潡潡潡ю潡ヰ潡潡︺��
+鉃�
+鉃� *${prefix}linkgp*
+鉃� *${prefix}simih [1/0]*
+鉃� *${prefix}marcar*
+鉃� *${prefix}add [@]*
+鉃� *${prefix}banir [@]*
+鉃� *${prefix}promover [@]*
+鉃� *${prefix}rebaixar*
+鉃� *${prefix}admins*
+鉃� *${prefix}marcar2*
+鉃� *${prefix}bc [texto]* (ele faz uma 鈩�)
+鉃� *${prefix}marcar3*
+鉃� *${prefix}bloqueados*
+鉃� *${prefix}bloquear [@]*
+鉃� *${prefix}desbloquear [@]*
+鉃� *${prefix}limpar*
+鉃� *${prefix}bc [ *texto* ]*
+鉃� *${prefix}bemvindo [1/0]*
+鉃� *${prefix}clonar [@]*
+鉃� *${prefix}help1*
+鉃� *${prefix}dono*
+鉃� *${prefix}owner*
+鉃� *${prefix}tts [texto]*
+鉃� *${prefix}setnome*
+鉃� *${prefix}termux*
+鉃� *${prefix}setfoto*
+鉃� *${prefix}grupoinfo*
+鉃� *${prefix}ytmp4*
+鉃� *${prefix}bomdia*
+鉃� *${prefix}boanoite*
+鉃� *${prefix}marcar*
+鉃� *${prefix}marcar2*
+鉃� *${prefix}marcar3*
+鉃�
+鉃€�� 饾棞饾棤饾棓饾棜饾棙饾棥饾棪 銆�
+鉃�
+鉃� *${prefix}loli* [off]
+鉃� *${prefix}loli1*
+鉃� *${prefix}hentai*
+鉃� *${prefix}dono*
+鉃� *${prefix}porno*
+鉃� *${prefix}boanoite*
+鉃� *${prefix}bomdia*
+鉃� *${prefix}boatarde*
+鉃� *${prefix}mia [aleat贸rias]*
+鉃� *${prefix}rize [aleat贸rias]*
+鉃� *${prefix}minato [aleat贸rias]*
+鉃� *${prefix}boruto [aleat贸rias]*
+鉃� *${prefix}hinata [aleat贸rias]*
+鉃� *${prefix}sasuke [aleat贸rias]*
+鉃� *${prefix}sakura [aleat贸rias]*
+鉃� *${prefix}naruto [aleat贸rias]*
+鉃� *${prefix}meme*   
+鉃� *${prefix}lofi*
+鉃� *${prefix}malkova*
+鉃� *${prefix}canal*
+鉃� *${prefix}nsfwloli1*
+鉃� *${prefix}reislin*
+鉃�
+鉃€�� 饾棞饾棥饾棫饾棙饾棢饾棞饾棜饾棙虃饾棥饾棖饾棞饾棓 饾棞饾棓 銆�
+鉃�
+鉃� *${prefix}simih 1 (para ativar)*
+鉃� *${prefix}simih 0 (para desativar)*
+鉃� *${prefix}simi (sua mensagem)*
+鉃�
+鉃€�愷潡橉潡� 饾棫饾棙饾棪饾棫饾棙銆�
+鉃�
+鉃� *${prefix}*
+鉃� *${prefix}*
+鉃� *${prefix}*
+鉃�
+鉃€�愷潡ｐ潡ヰ潡橉潡狆潡滒潡潡犮��
+鉃�
+鉃� *${prefix}dado*
+鉃� *${prefix}cekvip*
+鉃� *${prefix}premiumlist*
+鉃� *${prefix}delete*
+鉃� *${prefix}modapk*
+鉃� *${prefix}indo10*
+鉃� *${prefix}daftarvip [para virar Premium]*
+鉃� *${prefix}qrcode*
+鉃� *${prefix}chentai*
+鉃� *${prefix}gcpf*
+鉃� *${prefix}gbin*
+鉃� *${prefix}pack*
+鉃� *${prefix}destrava*
+鉃� *${prefix}gpessoa*
+
+鉃€�愷潡氿潡ヰ潡潡ｐ潡€��
+鉃�
+鉃� *${prefix}banir*
+鉃� *${prefix}leveling [on/off]*
+鉃� *${prefix}level*
+鉃� *${prefix}add*
+鉃� *${prefix}promover*
+鉃� *${prefix}setfoto [na legenda]*
+鉃� *${prefix}setname [texto]*
+鉃� *${prefix}rebaixar*
+鉃� *${prefix}admins*
+鉃� *${prefix}marcar*
+鉃� *${prefix}marcar2*
+鉃� *${prefix}marcar3*
+鉃� *${prefix}bemvindo [1/0]*
+鉃� *${prefix}grupoinfo*
+鉃� *${prefix}bomdia*
+鉃� *${prefix}boatarde*
+鉃� *${prefix}boanoite*
+鉃� *${prefix}setdesc*
+鉃� *${prefix}bug [sua mensagem]*
+鉃�
+鉃€�愷潡橉潡︷潡ｐ潡橉潡栶潡滒潡欚潡滒潡栶潡� 饾棗饾棦 饾棔饾棦饾棫銆戰潡�
+鉃�
+鉃� *${prefix}bug [sua mensagem]*
+鉃� *${prefix}clonar [@]*
+鉃� *${prefix}dono*
+鉃� *${prefix}ping [ver velocidade do bot]*
+鉃� *${prefix}termux*
+鉃� *${prefix}gay [@]*
+鉃� *${prefix}wame*
+鉃� *${prefix}map (nome)*
+鉃� *${prefix}setppbot (marque uma img)*
+鉃� *${prefix}pinterest (nome)*
+鉃� *${prefix}desligar (so para o dono)*
+鉃� *${prefix}timer*
+鉃�
+鉃€�愷潡狆潡旔潡滒潡� 饾棓饾棢饾棜饾棬饾棥饾棪銆戰潡�
+鉃�
+鉃� *${prefix}neko*
+鉃� *${prefix}ttp [texto]*
+鉃� *${prefix}testime*
+鉃� *${prefix}tomp3*
+鉃� *${prefix}modoanime [on/off]*
+鉃� *${prefix}modonsfw [on/off]*
+鉃� *${prefix}happymod [jogo/app]*
+鉃� *${prefix}rize*
+鉃� *${prefix}ytsearch*
+鉃� *${prefix}moddroid [jogo/app]*
+鉃� *${prefix}xvideos [titulo]**
+鉃� *${prefix}nomegp*
+鉃� *${prefix}nabutojokes (memes aleat贸rios)*
+鉃� *${prefix}animecry*
+鉃� *${prefix}gay1*
+鉃� *${prefix}next*
+鉃� *${prefix}alerta*
+鉃� *${prefix}belle [img aleat贸rias]*
+鉃� *${prefix}pronomeneu [texto]*
+鉃� *${prefix}hobby*
+鉃� *饾棥饾棦饾棤饾棙: ChiquinhoBot
+鉃� *饾棯饾棧饾棧: wa.me/+554184537851
+銆� CHIQUINHO 銆�
+鉃€�� 饾棖饾棦饾棤饾棓饾棥饾棗饾棦饾棪 饾棗饾棙 饾棤饾棬饾棪饾棞饾棖饾棓 銆�
+
+鉃� *${prefix} em teste
+鉃� *${prefix}jogaroxo*
+鉃� *${prefix} em teste
+鉃� *${prefix} em teste
+鉃� *${prefix}narutinho*
+鉃� *${prefix}}tobi*
+鉃� *${prefix}rapL*
+鉃� *${prefix}paypal*
+鉃� *${prefix}sad*
+
+鉃€�� 饾棦饾棬饾棫饾棩饾棦饾棪 /2 銆�
+
+*鉃� *${prefix}antilink [1/0]*
+*鉃� *${prefix}brainly [pergunta]*
+鉃� *${prefix}antiracismo [on/off]*
+鉃� *${prefix}setnomebot*
+鉃� *${prefix}meme*
+
+鉃€�� 饾棞饾棥饾棫饾棙饾棩饾棓饾棫饾棞饾棭饾棦饾棪 銆�
+
+NOTA 禄
+Mandar a msg sem o prefixo
+
+
+鉃� *${prefix} *beat1*
+鉃� *${prefix} *beat2*
+鉃� *${prefix} *hentaisom* (erro)
+
+鉃€�� 饾棗饾棦饾棥饾棦 銆�
+
+ *饾棥饾棦饾棤饾棙: ChiquinhoBot
+ *饾棯饾棧饾棧: wa.me/+554184537851
+
+
+
+
+銆� ChiquinhoBot 銆慲
+}
+
+exports.help = help
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/+554184537851
+║
+╚═〘 Chiquinho 〙
+`
+}
+exports.iklan = iklan
